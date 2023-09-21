abb7ebb3df2ab191d685a7cd89fbd47f7bcb3252
shem <shem.masoo@student.moringaschool.com>
2023-09-21 11:01:23 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+
+companyName = "Scuber"
+const companyName = "Scuber";
+ console.log (companyName);
+
+ mostProfitableNeighborhood = "chelsea";
+ 
+ let mostProfitableNeighborhood ="chelsea";
+  console.log (mostProfitableNeighborhood)
+
+
+
+  
